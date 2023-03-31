c7de3df165b32c5589f862822535e57c9924aa78
Salim Akcakoca <salim.akcakoca@msn.com>
2023-03-31 18:21:04 +0200

    Use provided interface and log exception

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,7 +1,18 @@
-#include <fmt/core.h>
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "passwordvalidator.hpp"
 
 int main()
 {
-    fmt::print("Hello app");
+    try
+    {
+        PasswordValidator::isValid("");
+    }
+    catch(const std::exception& e)
+    {
+        const std::string errorText{ e.what() };
+        spdlog::critical(errorText);
+    }
+
     return 0;
 }
